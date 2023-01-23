81d70bf2c83decfb2064306943e09c57effab341
EviBera <beraevi@gmail.com>
2023-01-23 15:27:53 +0100

    get the firts 3 letters of the city

--- frontend/script.js
+++ frontend/script.js
@@ -3,10 +3,12 @@ const loadEvent = function() {
     // Write your JavaScript code after this line
   
 const rootElement = document.querySelector("#root");
-console.log('Hello!');
 rootElement.insertAdjacentHTML('beforeend', `<label for="place">Chose a city: </label><input type="text" id="city">`);
 
-
+const cityInputElement = document.querySelector("#city");
+cityInputElement.addEventListener('input', function(event) {
+  console.log(event.target.value, event.target.value.substring(0, 3))
+})
   
     // Write your JavaScript code before this line
   
