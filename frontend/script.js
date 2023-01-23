2a7b05642790580985c951f74a10b5bb0d85e249
EviBera <beraevi@gmail.com>
2023-01-23 11:56:05 +0100

    add server.js, index.html, script.js

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+const loadEvent = function() {
+  
+    // Write your JavaScript code after this line
+  
+
+  
+  
+    // Write your JavaScript code before this line
+  
+  }
+  
+  window.addEventListener("load", loadEvent);
