12928060d3afa013709d08d568804bebb568f014
paszgyul <paszgyu@gmail.com>
2023-01-23 15:53:08 +0100

    Connect city with JSON

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,52 @@
+let fetchURL = "http://api.weatherapi.com/v1/current.json?key=bbe8663248294c28b60135807232301&q=", city="Budapest";
+
+
+
+
+
 const loadEvent = function() {
   
     // Write your JavaScript code after this line
   
-const rootElement = document.querySelector("#root");
-rootElement.insertAdjacentHTML('beforeend', `<label for="place">Chose a city: </label><input type="text" id="city">`);
+    let fetchWeather = (fetchURL) =>{
+        fetch(fetchURL)
+            .then(function(response) {
+                return response.json();
+                })
+            .then((data) => {
+                let datas = data;
+                
+                console.log(data);
+                
+                })
+            .catch(function(error) {
+                console.log(error);
+                });
+        }
 
-const cityInputElement = document.querySelector("#city");
-cityInputElement.addEventListener('input', function(event) {
-  console.log(event.target.value, event.target.value.substring(0, 3))
-})
+    fetchWeather(fetchURL+city);
+  
+
+const selectElement = document.querySelector('#cities');
+
+selectElement.addEventListener('change', (event) => {
+    console.log(event.target.value);
+    city = event.target.value;
+    fetchURL = "http://api.weatherapi.com/v1/current.json?key=bbe8663248294c28b60135807232301&q="+city;
+    fetchWeather(fetchURL);
+});
+  
+
+
+
+
+
   
     // Write your JavaScript code before this line
   
   }
+
+
+
   
   window.addEventListener("load", loadEvent);
