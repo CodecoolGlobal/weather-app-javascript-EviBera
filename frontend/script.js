d42873c246832cea890f6e0370b86509fe884277
EviBera <beraevi@gmail.com>
2023-01-23 15:08:46 +0100

    add basic city chooser input field

--- frontend/script.js
+++ frontend/script.js
@@ -2,8 +2,11 @@ const loadEvent = function() {
   
     // Write your JavaScript code after this line
   
+const rootElement = document.querySelector("#root");
+console.log('Hello!');
+rootElement.insertAdjacentHTML('beforeend', `<label for="place">Chose a city: </label><input type="text" id="city">`);
 
-  
+
   
     // Write your JavaScript code before this line
   
