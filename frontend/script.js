4b132ca721f2777f2e3de2421b9a43e322e73b6b
EviBera <beraevi@gmail.com>
2023-01-24 12:49:33 +0100

    add createDataList function

--- frontend/script.js
+++ frontend/script.js
@@ -2,8 +2,6 @@ let fetchURL = "http://api.weatherapi.com/v1/current.json?key=bbe8663248294c28b6
 
 
 
-
-
 const loadEvent = function() {
   
     // Write your JavaScript code after this line
@@ -14,32 +12,50 @@ const loadEvent = function() {
                 return response.json();
                 })
             .then((data) => {
-                let datas = data;
-                
                 console.log(data);
-                
+                document.querySelector("#citynames").innerHTML = '';
+                createDataList(data);
                 })
             .catch(function(error) {
                 console.log(error);
                 });
         }
 
-    fetchWeather(fetchURL+city);
+    //fetchWeather(fetchURL+city);
   
+
+const rootElement = document.querySelector("#root");
+rootElement.insertAdjacentHTML('beforeend', 
+    `<label for="choice">Choose a city:</label>
+    <input list="citynames" id="cities" name="city"></input>
+    <datalist id="citynames"></datalist>`
+  );
 
 const selectElement = document.querySelector('#cities');
 
-selectElement.addEventListener('change', (event) => {
+/* selectElement.addEventListener('change', (event) => {
     console.log(event.target.value);
     city = event.target.value;
     fetchURL = "http://api.weatherapi.com/v1/current.json?key=bbe8663248294c28b60135807232301&q="+city;
     fetchWeather(fetchURL);
-});
+}); */
   
 
+selectElement.addEventListener('input', (event) => {
+  console.log(event.target.value);
+  console.log(event.target.value.substring(0,3));
 
+  if (event.target.value.length > 2) {
+    city = event.target.value;
+    fetchURL = "http://api.weatherapi.com/v1/search.json?key=bbe8663248294c28b60135807232301&q="+city;
+    fetchWeather(fetchURL);
+  }
+});
 
-
+const createDataList = (data) => {
+  data.map(element => document.querySelector("#citynames").insertAdjacentHTML('beforeend', `<option value=${element["name"]}></option>`));
+  
+};
 
   
     // Write your JavaScript code before this line
