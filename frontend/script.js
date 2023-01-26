5b890ab58eaab6d9d78faed5290ad6e22e9c9228
paszgyul <paszgyu@gmail.com>
2023-01-26 14:24:42 +0100

    Add codes

--- frontend/script.js
+++ frontend/script.js
@@ -1,22 +1,20 @@
-let fetchURL = "", city="";
+let fetchURL = "", city="", origImg, favArray = [];
 
 const rootElement = document.querySelector("#root");
 
-rootElement.insertAdjacentHTML('beforeend', 
-    `<label for="choice">Choose a city:</label>
-    <input list="citynames" id="cities" name="city"></input>
-    <datalist id="citynames"></datalist>`
-  );
-
-rootElement.insertAdjacentHTML('beforeend', `<div id="pic"></div>`);
+rootElement.insertAdjacentHTML('beforeend', `<div id="pic" class="container-flex "></div>`);
 const pictureElement = document.querySelector("#pic");
 
-rootElement.insertAdjacentHTML('beforeend', `<div hidden id="spinner"></div>`);
+pictureElement.insertAdjacentHTML('beforeend', `<div hidden id="spinner"></div>`);
 const spinner = document.getElementById("spinner");
 
+pictureElement.insertAdjacentHTML('beforeend', 
+    `<div class="row m-0 p-0"><div class="col-12 p-3">
+    <input list="citynames" id="cities" name="city" placeholder="Choose a city"></input>
+    <datalist id="citynames"></datalist></div></div>`
+  );
+
 const selectElement = document.querySelector('#cities');
-
-
 
 const loadEvent = function() {
 
@@ -25,21 +23,33 @@ const loadEvent = function() {
 
     fetch("https://api.pexels.com/v1/search?query=" + theme, { 
       headers: {
-        Authorization: 'BQJqfCnx2tJDWXIKI7kZqBEwj4yn7pYWoyAZ9nlHtmH7tmmrmKr0HeHa'
+        Authorization: 'KxcTUGcE6J6QwkzCc4K53QUcAlbI1VYtvXJzJwCuombgqBlJHYsUh5uM'
       }
     })
     .then(resp => {
       return resp.json()
     })
     .then(data => {
-      console.log('photos: ');
-      console.log(data.photos);
-      let picture = (data.photos[Math.floor(Math.random() * 15)])
-      console.log('pic to the app: ');
-      console.log(picture);
-      console.log(picture.src);
-      pictureElement.innerHTML = '';
-      pictureElement.insertAdjacentHTML('afterbegin', `<img src=${picture.src.landscape} alt=${picture.alt}>`);
+      // console.log('photos: ');
+      // console.log(data.photos);
+      let picture = (data.photos[Math.floor(Math.random() * data.photos.length)])
+      // console.log('pic to the app: ');
+      // console.log(picture);
+      // console.log(picture.src);
+      //pictureElement.innerHTML = '';
+      //pictureElement.insertAdjacentHTML('afterbegin', `<img src=${picture.src.landscape} alt=${picture.alt}>`);
+      theme == "winter landscape" ? origImg = picture.src.original : "";
+     
+      if(typeof(picture) !== "undefined")
+        {
+        pictureElement.style.background = `transparent url(${picture.src.original}) no-repeat center center`;
+        }
+      else
+        {
+        pictureElement.style.background = `transparent url(${origImg}) no-repeat center center`;
+        }
+      pictureElement.style.backgroundSize = 'cover';
+      // note.style.color = 'red';
     });
     
   };
@@ -58,31 +68,38 @@ const loadEvent = function() {
       .then((data) => {
 
         if (fetchURL.includes('search')){
-          console.log('search data: ' + data);
+          // console.log('search data: ' + data);
           document.querySelector("#citynames").innerHTML = '';
           createDataList(data);
         };
 
         if (fetchURL.includes('current')){
           spinner.setAttribute('hidden', '');
-                  console.log("weather")
-                  console.log("current data: " + data);
+                  //console.log("weather")
+                  //console.log("current data: " + data);
                   displayWeather(data);
-                  console.log(data);
+                  //console.log(data);
         };
 
       })
             .catch(function(error) {
-                console.log(error);
+                //console.log(error);
             });
     };
 
   
   
   selectElement.addEventListener('input', (event) => {
-    console.log(event.target.value);
-    console.log(event.target.value.substring(0,3));
+    //console.log(event.target.value);
+    //console.log(event.target.value.substring(0,3));
   
+    if (event.target.value.length < 2) {
+      console.log(favArray);
+      document.querySelector("#citynames").innerHTML = "";
+      createDataList(favArray);
+      }
+
+
     if (event.target.value.length > 2) {
       city = event.target.value;
       fetchURL = "http://api.weatherapi.com/v1/search.json?key=bbe8663248294c28b60135807232301&q="+city;
@@ -92,6 +109,7 @@ const loadEvent = function() {
 
 
   const createDataList = (data) => {
+    console.log(data);
     data.map(element => document.querySelector("#citynames").insertAdjacentHTML('beforeend', `<option value=${element["name"]}></option>`));
   };
 
@@ -106,6 +124,9 @@ const loadEvent = function() {
   });
 
 
+
+
+
   const wrapContent = (tag, text) => {
     return `<${tag}>${text}</${tag}>`
   };
@@ -113,12 +134,12 @@ const loadEvent = function() {
 
   const displayWeather = (data) => {
     if (!document.querySelector("#card")){
-      rootElement.insertAdjacentHTML('beforeend', `<div id="card"></div>`);
+      pictureElement.insertAdjacentHTML('beforeend', `<div class="row"><div class="col-4"></div><div class="col-4"><div id="card" class="p-3"></div></div><div class="col-4"></div></div>`);
     }
     const cardElement = document.querySelector("#card");
     cardElement.innerHTML = '';
     
-    let textToHTML = wrapContent('div', ('city: ' + '<strong>' + data.location.name + '</strong>')) +
+    let textToHTML = wrapContent('div', ('city: ' + '<strong>' + data.location.name + '</strong>' + '<span id="addFav" class="star empty">&starf;</span>')) +
       wrapContent('div', ('region: ' + data.location.region + ' / country: ' +data.location.country)) +
       `<img src="${data.current.condition.icon}" alt="little icon">` +
       wrapContent('div', ('sky conditions: ' + data.current.condition.text)) +
@@ -130,8 +151,16 @@ const loadEvent = function() {
       wrapContent('div', ('humidity: ' + data.current.humidity + ' %')) +
       wrapContent('div', ('pressure: ' + data.current.pressure_mb + ' hPa'));
 
-
     cardElement.insertAdjacentHTML('afterbegin', textToHTML);
+
+    if(document.querySelector("#addFav"))
+      {
+      document.querySelector("#addFav").addEventListener('click', (event) => {
+        if (!favArray.includes(data.location.name)) {
+          favArray.push({name : data.location.name});
+        }
+        });
+      }
     
   };
 
